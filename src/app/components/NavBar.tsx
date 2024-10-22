7c9e04b12a2f95caae456d5211060d922a904573
amhguido <guidohdezz29@gmail.com>
2024-10-22 14:11:42 -0600

    NavBar Actualizado

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,12 +1,14 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa'; // Icono de hamburguesa
 
 type NavbarProps = {
-  onLocationChange: (location: string) => void; // Cambiado a onLocationChange
+  onLocationChange: (location: string) => void;
 };
 
 const Navbar: React.FC<NavbarProps> = ({ onLocationChange }) => {
   const [locationInput, setLocationInput] = useState<string>('');
+  const [menuOpen, setMenuOpen] = useState<boolean>(false); // Estado para el menú móvil
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLocationInput(e.target.value);
@@ -14,7 +16,7 @@ const Navbar: React.FC<NavbarProps> = ({ onLocationChange }) => {
 
   const handleSearch = () => {
     if (locationInput.trim()) {
-      onLocationChange(locationInput); // Usa onLocationChange para enviar la ubicación
+      onLocationChange(locationInput);
       setLocationInput('');
     }
   };
@@ -22,21 +24,36 @@ const Navbar: React.FC<NavbarProps> = ({ onLocationChange }) => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
+        {/* Logo y título */}
         <div className="flex items-center space-x-2">
-          <img 
-            src="/images/weather-news_648198.png" 
-            alt="WhatsMyWeather Logo" 
-            className="h-8 w-8" 
+          <img
+            src="/images/weather-news_648198.png"
+            alt="WhatsMyWeather Logo"
+            className="h-8 w-8"
           />
           <div className="text-white font-bold text-xl font-roboto">WhatsMyWeather</div>
         </div>
-        <div className="flex space-x-4">
+
+        {/* Botón de hamburguesa para pantallas pequeñas */}
+        <button
+          className="block lg:hidden text-white"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FaBars size={24} />
+        </button>
+
+        {/* Input y botón de búsqueda (ocultable en móviles) */}
+        <div
+          className={`${
+            menuOpen ? 'block' : 'hidden'
+          } lg:flex space-x-4 items-center w-full lg:w-auto mt-4 lg:mt-0`}
+        >
           <input
             type="text"
             value={locationInput}
             onChange={handleInputChange}
             placeholder="Ej: Jerusalem, Israel"
-            className="p-2 rounded w-64 text-black"
+            className="p-2 rounded w-full lg:w-64 text-black"
           />
           <button
             onClick={handleSearch}
